026b45b0fe7905cd99d7e105e5b9104622ed6d22
JerryQ <phamnhat05111997@gmail.com>
2023-05-27 00:09:34 +0700

    init project

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  theme: {
+    extend: {
+      screens: {
+        'sm': '640px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1280px',
+        '2xl': '1536px',
+      },
+      colors: {
+        mainColor: '#60B301',
+        timKiem: '#BDBDBD',
+        textColor: '#464646'
+      },
+      backgroundImage: {
+        'banner1': "url('./images/banner1.jpg')",
+        'banner2': "url('./images/banner2.jpg')",
+        'banner3': "url('./images/banner3.jpg')"
+      }
+    }
+  }
+}
