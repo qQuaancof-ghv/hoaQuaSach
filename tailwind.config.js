608444846c52a31668f2380883c99a4fa0b4182f
Jerry <phamnhat05111997@gmail.com>
2023-05-31 17:01:18 +0700

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,15 +10,26 @@ module.exports = {
       },
       colors: {
         mainColor: '#60B301',
+        bdBox:'#78c225',
         timKiem: '#BDBDBD',
         textColor: '#464646',
-        btn: '#F2F2F2'
+        btn: '#F2F2F2',
+        productText: '#AEA2A2',
+        productTitle: '#3A3A3A',
+        bdImg: '#E0E0E0',
+        textKM: '#4B4B4B',
+        textBG: 'rgba(196, 196, 196, 0.51)'
       },
       backgroundImage: {
         'banner1': "url('./images/banner1.jpg')",
         'banner2': "url('./images/banner2.jpg')",
-        'banner3': "url('./images/banner3.jpg')"
-      }
+        'banner3': "url('./images/banner3.jpg')",
+        'bannerKM': "url('./images/item5.jpg')",
+        'bannerMain': "url('./images/bannerMain.png')",
+      },
+      boxShadow: {
+        '3xl': '0px 4px 4px rgba(0, 0, 0, 0.25)',
+      },
     }
   }
 }
