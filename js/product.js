5818d310d13f3d7b8e501504c5a1a4b569436e09
JerryQ <phamnhat05111997@gmail.com>
2023-06-06 15:46:34 +0700

    init update

--- js/product.js
+++ js/product.js
@@ -1,3 +1,8 @@
 $(document).ready(function() {
-    
-  });
+    $(".product-header").children('span').addClass('isOpen');
+    $(".product-header").siblings().addClass('isOpen');
+    $(".product-header").click(function() {
+       $(this).children('span').toggleClass('isOpen');
+       $(this).siblings().toggleClass('isOpen');
+    })
+});
