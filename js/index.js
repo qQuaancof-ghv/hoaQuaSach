c068ebc08dd97620a813a3ee3158d044a737337b
JerryQ <phamnhat05111997@gmail.com>
2023-06-07 16:50:40 +0700

    init update

--- js/index.js
+++ js/index.js
@@ -1,46 +1,68 @@
-$(document).ready(function(){
-  
-// owlCarousel
-$(".owl-one").owlCarousel({
-  loop: true,
-  margin: 10,
-  nav: true,
-  autoplay: true,
-  autoplayTimeout: 3000,
-  autoplayHoverPause: true,
-  responsive: {
-    0: {
-      items: 1,
-      dots: false,
-    },
-    600: {
-      items: 1,
-    },
-    1000: {
-      items: 1,
-    },
-  },
-});
+$(document).ready(function () {
 
-$(".owl-two").owlCarousel({
-  loop: true,
-  margin: 10,
-  nav: false,
-  autoplay: true,
-  autoplayTimeout: 3000,
-  responsive: {
-    0: {
-      items: 1,
-      dots: false,
+  // owlCarousel
+  $(".owl-one").owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    autoplayHoverPause: true,
+    responsive: {
+      0: {
+        items: 1,
+        dots: false,
+      },
+      600: {
+        items: 1,
+      },
+      1000: {
+        items: 1,
+      },
     },
-    600: {
-      items: 2,
-      dots: false,
+  });
+
+  $(".owl-two").owlCarousel({
+    loop: true,
+    margin: 20,
+    nav: false,
+    dots: false,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 2,
+      },
+      1000: {
+        items: 3,
+      },
     },
-    1000: {
-      items: 3,
-      dots: false,
-    },
-  },
-});
+  });
+
+  $(".owl-carousel").owlCarousel({
+    loop: true, 
+    margin: 20, 
+    autoplay: true,
+    autoplayTimeout: 3000,
+    nav: true, 
+    dots: false,
+    responsive: {
+      0: {
+        items: 1 
+      },
+      600: {
+        items: 2 
+      },
+      1000: {
+        items: 3 
+      },
+      1400: {
+        items: 4
+      }
+    }
+  });
+  
 })
